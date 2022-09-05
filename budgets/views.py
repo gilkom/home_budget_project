28a60fcb2be04086cb260f79e429c045f7c8031e
gko <marcingilko@gmail.com>
2022-09-05 20:02:45 +0200

    add @login_required on views

--- budgets/views.py
+++ budgets/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from .forms import ExpenditureForm, ExpenditureEditForm
@@ -10,7 +11,7 @@ def index(request):
     """Main page for budgets app."""
     return render(request, 'budgets/index.html')
 
-
+@login_required
 def expenses(request):
     """Displaying all expenses from current period."""
     expenses = BudgetsExpenditure.objects.all().order_by('expenditure_id')
@@ -26,7 +27,7 @@ def expenses(request):
     context = {'form': form, 'expenses': expenses}
     return render(request, 'budgets/expenses.html', context)
 
-
+@login_required()
 def expenditure(request, expenditure_id):
     """Displaying, editing and deleting details of a specified expense."""
     expenditure = BudgetsExpenditure.objects.get(expenditure_id=expenditure_id)
